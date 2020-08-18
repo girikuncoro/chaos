00c6d900bea3768760f77f6dad8d25d75011df97
Giri Kuncoro <girikuncoro@users.noreply.github.com>
2020-08-18 15:28:09 +0700

    Implement build method for kubeclient and deploy litmus (#3)

--- pkg/kube/factory.go
+++ pkg/kube/factory.go
@@ -1,8 +1,10 @@
 package kube
 
 import (
+	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/kubectl/pkg/validation"
 )
 
 // Factory provides abstractions that allow kubectl command to be extended
@@ -12,4 +14,8 @@ type Factory interface {
 	ToRawKubeConfigLoader() clientcmd.ClientConfig
 	// KubernetesClientSet gives back an external clientset
 	KubernetesClientSet() (*kubernetes.Clientset, error)
+	// NewBuilder implements common patterns for CLI interactions with generic resources.
+	NewBuilder() *resource.Builder
+	// Validator returns schema that can validate objects.
+	Validator(validate bool) (validation.Schema, error)
 }
--- pkg/kube/interface.go
+++ pkg/kube/interface.go
@@ -1,12 +1,15 @@
 package kube
 
+import "io"
+
 // Interface represents a client that communicates with Kubernetes API.
 //
 // KubernetesClient must be concurrency safe.
 type Interface interface {
 	// Create creats one or more resources.
 	Create(resources ResourceList) (*Result, error)
-
+	// Build creates a resource list from a Reader
+	Build(reader io.Reader, validate bool) (ResourceList, error)
 	// IsReachable checks whether the client is able to connect to the cluster
 	IsReachable() error
 }
