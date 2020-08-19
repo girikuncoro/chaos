23571f66aed7a3d88164eff774b1bc29d465b30d
Giri Kuncoro <girikuncoro@gmail.com>
2020-08-19 11:28:11 +0700

    Add litmus rbac and dynamic options to manifests

--- cmd/chaos/installer/manifests.go
+++ cmd/chaos/installer/manifests.go
@@ -3,14 +3,17 @@ package installer
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	rbacv1beta1 "k8s.io/api/rbac/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/yaml"
 )
 
 const (
-	deploymentName     = "chaos-operator-ce"
-	serviceAccountName = "litmus"
+	deploymentName         = "chaos-operator"
+	serviceAccountName     = "litmus"
+	clusterRoleName        = "litmus"
+	clusterRoleBindingName = "litmus"
 )
 
 // LitmusManifests gets the Deployment, ServiceAccount manifests
@@ -21,12 +24,11 @@ func LitmusManifests(opts *Options) ([]string, error) {
 
 	ns := Namespace(opts.Namespace)
 	sa := ServiceAccount(opts.Namespace)
-	dep, err := Deployment(opts)
-	if err != nil {
-		return []string{}, err
-	}
+	cr := ClusterRole()
+	crb := ClusterRoleBinding(opts.Namespace)
+	dep := Deployment(opts)
 
-	objs := []runtime.Object{ns, sa, dep}
+	objs := []runtime.Object{ns, sa, cr, crb, dep}
 
 	manifests := make([]string, len(objs))
 	for i, obj := range objs {
@@ -36,7 +38,7 @@ func LitmusManifests(opts *Options) ([]string, error) {
 		}
 		manifests[i] = string(o)
 	}
-	return manifests, err
+	return manifests, nil
 }
 
 // Namespace gets a namespace object that can be used to generate
@@ -55,6 +57,70 @@ func Namespace(name string) *v1.Namespace {
 		},
 	}
 	return ns
+}
+
+// ClusterRole gets a cluster role object that can be used to generate
+// manifest as string.
+func ClusterRole() *rbacv1beta1.ClusterRole {
+	cr := generateClusterRole()
+	cr.TypeMeta = metav1.TypeMeta{
+		APIVersion: "rbac.authorization.k8s.io/v1beta1",
+		Kind:       "ClusterRole",
+	}
+	return cr
+}
+
+func generateClusterRole() *rbacv1beta1.ClusterRole {
+	labels := generateLabels(map[string]string{"name": clusterRoleName})
+	cr := &rbacv1beta1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   clusterRoleName,
+			Labels: labels,
+		},
+		Rules: []rbacv1beta1.PolicyRule{
+			{
+				APIGroups: []string{"", "apps", "batch", "litmuschaos.io", "apps.openshift.io"},
+				Resources: []string{"pods", "jobs", "deployments", "replicationcontrollers", "daemonsets", "replicasets", "statefulsets", "deploymentconfigs", "events", "configmaps", "services", "secrets", "chaosengines", "chaosexperiments", "chaosresults"},
+				Verbs:     []string{"get", "create", "update", "patch", "delete", "list", "watch", "deletecollection"},
+			},
+			{
+				APIGroups: []string{"admissionregistration.k8s.io"},
+				Resources: []string{"validatingwebhookconfigurations"},
+				Verbs:     []string{"get", "create", "list", "delete", "update"},
+			},
+		},
+	}
+	return cr
+}
+
+// ClusterRoleBinding gets a cluster role binding object that can be used to generate
+// manifest as string.
+func ClusterRoleBinding(serviceAccountNamespace string) *rbacv1beta1.ClusterRoleBinding {
+	crb := &rbacv1beta1.ClusterRoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "rbac.authorization.k8s.io/v1beta1",
+			Kind:       "ClusterRoleBinding",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: clusterRoleBindingName,
+			Labels: map[string]string{
+				"name": clusterRoleBindingName,
+			},
+		},
+		RoleRef: rbacv1beta1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     clusterRoleName,
+		},
+		Subjects: []rbacv1beta1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      serviceAccountName,
+				Namespace: serviceAccountNamespace,
+			},
+		},
+	}
+	return crb
 }
 
 // ServiceAccount gets a service account object that can be used to generate
@@ -78,19 +144,16 @@ func ServiceAccount(namespace string) *v1.ServiceAccount {
 
 // Deployment gets a deployment object that can be used to generate
 // manifest as string.
-func Deployment(opts *Options) (*appsv1.Deployment, error) {
-	dep, err := generateDeployment(opts)
-	if err != nil {
-		return nil, err
-	}
+func Deployment(opts *Options) *appsv1.Deployment {
+	dep := generateDeployment(opts)
 	dep.TypeMeta = metav1.TypeMeta{
 		APIVersion: "apps/v1",
 		Kind:       "Deployment",
 	}
-	return dep, nil
+	return dep
 }
 
-func generateDeployment(opts *Options) (*appsv1.Deployment, error) {
+func generateDeployment(opts *Options) *appsv1.Deployment {
 	labels := generateLabels(map[string]string{"name": "chaos-operator"})
 	d := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
@@ -99,6 +162,7 @@ func generateDeployment(opts *Options) (*appsv1.Deployment, error) {
 			Labels:    labels,
 		},
 		Spec: appsv1.DeploymentSpec{
+			Replicas: opts.getReplicas(),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: labels,
 			},
@@ -107,17 +171,33 @@ func generateDeployment(opts *Options) (*appsv1.Deployment, error) {
 					Labels: labels,
 				},
 				Spec: v1.PodSpec{
+					ServiceAccountName: serviceAccountName,
 					Containers: []v1.Container{
 						{
-							Name:  "chaos-operator",
-							Image: "litmuschaos/chaos-operator:1.7.0",
+							Name:            "chaos-operator",
+							Image:           opts.getImage(),
+							Command:         []string{"chaos-operator"},
+							ImagePullPolicy: v1.PullAlways,
+							Env: []v1.EnvVar{
+								{Name: "CHAOS_RUNNER_IMAGE", Value: opts.getImage()},
+								{Name: "WATCH_NAMESPACE", Value: ""},
+								{
+									Name: "POD_NAME",
+									ValueFrom: &v1.EnvVarSource{
+										FieldRef: &v1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
+										},
+									},
+								},
+								{Name: "OPERATOR_NAME", Value: "chaos-operator"},
+							},
 						},
 					},
 				},
 			},
 		},
 	}
-	return d, nil
+	return d
 }
 
 func generateLabels(labels map[string]string) map[string]string {
--- cmd/chaos/installer/options.go
+++ cmd/chaos/installer/options.go
@@ -11,4 +11,21 @@ type Options struct {
 	Namespace string
 	// ImageSpec identifies the image Litmus will use when deployed.
 	ImageSpec string
+	// Replicas identify number of chaos-operator instances to run on the cluster
+	Replicas int
+}
+
+func (opts *Options) getReplicas() *int32 {
+	replicas := int32(1)
+	if opts.Replicas > 1 {
+		replicas = int32(opts.Replicas)
+	}
+	return &replicas
+}
+
+func (opts *Options) getImage() string {
+	if opts.ImageSpec == "" {
+		return defaultImage + ":latest"
+	}
+	return opts.ImageSpec
 }
