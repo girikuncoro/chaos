172d69cd9e100eb53ca8217fb9303dfab501b331
Giri Kuncoro <girikuncoro@gmail.com>
2020-08-21 23:31:14 +0700

    Implement litmus chaos storage

---
+++ pkg/storage/storage.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"github.com/girikuncoro/chaos/pkg/chaostest"
+	"github.com/girikuncoro/chaos/pkg/storage/driver"
+)
+
+// Storage represents a storage engine for a ChaosTest.
+type Storage struct {
+	driver.Driver
+	Log func(string, ...interface{})
+}
+
+// ListChaosTests returns all chaos tests from storage.
+func (s *Storage) ListChaosTests() ([]*chaostest.ChaosTest, error) {
+	s.Log("listing all chaos tests in storage")
+	return s.Driver.List()
+}
